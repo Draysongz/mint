1e65012a32048c438493dbdc8001689e06f348e7
draysongz <dharmiegra4@gmail.com>
2024-02-13 17:25:26 +0100

    done

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -12,7 +12,7 @@ use mpl_token_metadata::{
     state::DataV2,
 };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("F72tM7HzRFdKHbRAjTkWPD3QHgMUSoY8xX9RzvrU7zS9");
 #[program]
 pub mod solana_nft_anchor {
 
